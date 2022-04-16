2747c52a2f7bdcc980beb2faf4e720eeb6cfe56e
shenyehuijie <shenyehuijie@yahoo.co.jp>
2022-04-16 13:57:31 +0900

    8th commit colered

--- app.py
+++ app.py
@@ -81,18 +81,25 @@ def total_business_days(today_dt, target_day_dt):
 def ret_date_prop(target_date2):
     target_date2_dt = datetime.datetime.strptime(target_date2, '%Y%m%d')
 
-    date_prop = '国民の祝日、または土日、年末年始に該当していません'
+    date_prop = '平日'
+    prop_ID = 'weekday'
 
     if jpholiday.is_holiday_name(target_date2_dt):
-        date_prop = jpholiday.is_holiday_name(target_date2_dt) + 'になっています。'
+        date_prop = jpholiday.is_holiday_name(target_date2_dt)
+        prop_ID = 'holiday'
 
     elif which_sun_or_sat(target_date2_dt):
-        date_prop = which_sun_or_sat(target_date2_dt) + 'になっています！！'
+        date_prop = which_sun_or_sat(target_date2_dt)
+        if date_prop == '土曜日':
+            prop_ID = 'sat'
+        else:
+            prop_ID = 'holiday'
 
     elif is_nenmatu(target_date2_dt):
-        date_prop = '年末年始になっています！！'
+        date_prop = '年末年始'
+        prop_ID = 'holiday'
 
-    return date_prop
+    return date_prop, prop_ID
 
 
 def main():
@@ -117,13 +124,23 @@ def main():
     kouki_date2 = cal_futaku_date2(lead_days, target_date2_dt)
     target_date2_str = datetime.datetime.strftime(target_date2_dt, '%Y年%m月%d日')
     kouki_date2_str = datetime.datetime.strftime(kouki_date2, '%Y年%m月%d日')
-    date_prop = ret_date_prop(target_date2)
+    date_prop, prop_ID = ret_date_prop(target_date2)
 
-    '指定日は：　　', date_prop
+    if prop_ID == 'weekday':
+        st.write(
+            '指定日は：　　'f'{date_prop}', '　　になっています', unsafe_allow_html=False)
+    elif prop_ID == 'holiday':
+        st.write(
+            '指定日は：　　'f'<span style="color:orangered">{date_prop}</span>', '　　になっています', unsafe_allow_html=True)
+    elif prop_ID == 'sat':
+        st.write(
+            '指定日は：　　'f'<span style="color:dodgerblue">{date_prop}</span>', '　　になっています', unsafe_allow_html=True)
+
     '付託の日：　　', target_date2_str
-    '工期初日：　　', kouki_date2_str, '　　以降が推奨されます。'
+    st.write(
+        '工期初日：　　'f'<span style="color:mediumseagreen">{kouki_date2_str}</span>', '　　以降が推奨されます。', unsafe_allow_html=True)
     '--------------------------------------------------------------------------------------'
-    ##来年度4月1日を初期値に指定##
+    #####################来年度4月1日を初期値に指定#######################
     month_today = today_dt.month
     year_today = today_dt.year
 
@@ -132,20 +149,30 @@ def main():
     else:
         next_year = datetime.date(year=year_today, month=4, day=1)
     next_year_str = datetime.datetime.strftime(next_year, '%Y%m%d')
-    #################
+    #############################################################
     target_date = st.text_input(
-        '工期初日から付託期日を算出する場合：　　(↓yyyymmdd形式で工期初日を指定)', value=next_year_str)
+        '工期初日から付託期日を割り出す場合：　　(↓yyyymmdd形式で工期初日を指定)', value=next_year_str)
     target_date_dt = datetime.datetime.strptime(target_date, '%Y%m%d')
 
     futaku_date = cal_futaku_date(lead_days, target_date_dt)
     futaku_date_str = datetime.datetime.strftime(futaku_date, '%Y年%m月%d日')
     target_date_str = datetime.datetime.strftime(target_date_dt, '%Y年%m月%d日')
     counter = total_business_days(today_dt, target_date_dt)
-    date_prop0 = ret_date_prop(target_date)
+    date_prop0, prop_ID0 = ret_date_prop(target_date)
 
-    '指定日は：　　', date_prop0
+    if prop_ID0 == 'weekday':
+        st.write(
+            '指定日は：　　'f'{date_prop0}', '　　になっています', unsafe_allow_html=False)
+    elif prop_ID0 == 'holiday':
+        st.write(
+            '指定日は：　　'f'<span style="color:orengered">{date_prop0}</span>', '　　になっています', unsafe_allow_html=True)
+    elif prop_ID0 == 'sat':
+        st.write(
+            '指定日は：　　'f'<span style="color:dodgerblue">{date_prop0}</span>', '　　になっています', unsafe_allow_html=True)
+
     '工期初日：　　', target_date_str, '　　まで', counter, '営業日'
-    '請負付託：　　', futaku_date_str, '　　までの実施が推奨されます。'
+    st.write(
+        '請負付託：　　'f'<span style="color:mediumseagreen">{futaku_date_str}</span>', '　　までの実施が推奨されます。', unsafe_allow_html=True)
     '--------------------------------------------------------------------------------------'
 
 
